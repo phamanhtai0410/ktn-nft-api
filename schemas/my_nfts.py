38ee0ed30850238f7eff14ea386f3f7a754e15cc
PTChinh <9a2.phamtranchinh@gmail.com>
2022-10-13 17:24:51 +0700

    Update select field of get my nfts

--- schemas/my_nfts.py
+++ schemas/my_nfts.py
@@ -16,11 +16,11 @@ class NFTResponseSchema(Schema):
     class Meta:
         unknown = EXCLUDE
 
-    owner = fields.String()
-    image = fields.String()
-    desc = fields.String()
-    price = fields.Decimal()
-    timestamp = fields.Integer(default=0)
+    token_id = fields.Integer()
+    address = fields.String()
+    rarity = fields.Integer()
+    token_uri = fields.String()
+    created_time = fields.DateTime()
 
 
 class MyNFTsResponseSchema(Schema):
