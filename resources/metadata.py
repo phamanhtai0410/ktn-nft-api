3bc2fdf47bb29b45a4c3fec1fce8e8d25a4289fb
PTChinh <9a2.phamtranchinh@gmail.com>
2022-10-20 11:28:11 +0700

    Update format cid to bytes32

--- resources/metadata.py
+++ resources/metadata.py
@@ -60,7 +60,7 @@ class MetaDataResource(Resource):
             _cid_hex = web3.Web3.toHex(bytes(_cid, 'utf-8'))
             _cid_hex_int = int(_cid_hex, 16)
             _padding = 32
-            _cids_bytes.append(f'{_cid_hex_int:<#0{_padding}x}')
+            _cids_bytes.append(f'{_cid_hex_int:<#0{_padding}x}'[:66])
             _cids.append(_cid)
             _rarities.append(get(_nft_detail, 'rarity'))
             _types.append(get(_nft_detail, 'type'))
