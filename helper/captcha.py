fcb615f54203203ae5262d352a94bd3d89b81f2f
khoabd <khoabd98@gmail.com>
2022-10-24 16:31:49 +0700

    [DEBUG] add log

--- helper/captcha.py
+++ helper/captcha.py
@@ -26,38 +26,34 @@ RECAPTCHA_ERROR_CODES = {
 class CaptchaHelper:
     @staticmethod
     def validate_recaptcha(response, remote_addr, action):
-        try:
-            """Performs the actual validation."""
-            # try:
-            #     private_key = Config.RECAPTCHA3_PRIVATE_KEY
-            # except KeyError:
-            #     raise RuntimeError("RECAPTCHA3_PRIVATE_KEY is not set in app config.")
+        """Performs the actual validation."""
+        # try:
+        #     private_key = Config.RECAPTCHA3_PRIVATE_KEY
+        # except KeyError:
+        #     raise RuntimeError("RECAPTCHA3_PRIVATE_KEY is not set in app config.")
 
-            data = {
-                'secret': Config.RECAPTCHA3_PRIVATE_KEY,
-                'remoteip': remote_addr,
-                'response': response
-            }
-            debug(f"Check captcha {data}")
-            http_response = requests.post(Config.RECAPTCHA_VERIFY_SERVER, data, timeout=10)
-            if http_response.status_code != 200:
-                return False
-            debug(f"Res check captcha {http_response.text}")
+        data = {
+            'secret': Config.RECAPTCHA3_PRIVATE_KEY,
+            'remoteip': remote_addr,
+            'response': response
+        }
+        debug(f"Check captcha {data}")
+        http_response = requests.post(Config.RECAPTCHA_VERIFY_SERVER, data, timeout=10)
+        if http_response.status_code != 200:
+            return False
+        debug(f"Res check captcha {http_response.text}")
 
-            json_resp = http_response.json()
-            if get(json_resp, 'success') and get(json_resp, 'action') == action:
-                debug(json_resp)
-                return True
-            else:
-                debug(json_resp)
+        json_resp = http_response.json()
+        if get(json_resp, 'success') and get(json_resp, 'action') == action:
+            debug(json_resp)
+            return True
+        else:
+            debug(json_resp)
 
-            for error in json_resp.get("error-codes", []):
-                if error in RECAPTCHA_ERROR_CODES:
-                    raise BadRequest(errors={
-                        'captcha': RECAPTCHA_ERROR_CODES[error]
-                    })
+        for error in json_resp.get("error-codes", []):
+            if error in RECAPTCHA_ERROR_CODES:
+                raise BadRequest(errors={
+                    'captcha': RECAPTCHA_ERROR_CODES[error]
+                })
 
-        except:
-            sentry_sdk.capture_exception()
-            traceback.print_exc()
         return False
