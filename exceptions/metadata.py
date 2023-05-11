a09ee060e75429e4cd944c857579966fd81f6829
Pham Anh Tai <phamanhtai0410@gmail.com>
2023-05-11 09:47:44 +0700

    [Comment the check whitelist discount]

--- exceptions/metadata.py
+++ exceptions/metadata.py
@@ -18,7 +18,7 @@ class UserNotInWhitelistEx(Exception):
     pass
 
 class NotMintStartTimeYetEx(Exception):
-    def __init__(self, msg='Have not start time yet', *args: object, **kwargs) -> None:
+    def __init__(self, msg='Currently Mint Not Active', *args: object, **kwargs) -> None:
         super().__init__(*args)
         self.status_code = 400
         self.msg = msg
